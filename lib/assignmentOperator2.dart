5269ccc19d0950582ba127b2344447e1d7803eef
Dart_Basic_Practice <soheldcc18@gmail.com>
2024-02-21 03:22:46 +0600

    initial commit

---
+++ lib/assignmentOperator2.dart
@@ -0,0 +1,22 @@
+void main(){
+
+  int x = 20;
+  int y = 30;
+
+  bool smaller, greater, equal, smallerOrEqual, greaterOrEqual, notEqual;
+
+  smaller = x < y;
+  greater = x > y;
+  equal = x == y;
+  smallerOrEqual = x <= y;
+  greaterOrEqual = x >= y;
+  notEqual = x != y;
+
+  print(smaller);
+  print(greater);
+  print(equal);
+  print(smallerOrEqual);
+  print(greaterOrEqual);
+  print(notEqual);
+
+}
---
+++ lib/boolean.dart
@@ -0,0 +1,14 @@
+void main(){
+  int numberOne = 10;
+  int numberTwo = 20;
+
+  print(numberOne > numberTwo);
+
+  print(numberTwo > numberOne);
+
+  print(numberOne / 2 == 0);
+
+  bool result = numberTwo > numberOne;
+  print(result);
+
+}
