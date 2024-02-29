abdd627ac1ca65bba1f9bbe8564bf93f68d01e39
Dart_Basic_Practice <soheldcc18@gmail.com>
2024-02-29 11:55:41 +0600

    initial commit

---
+++ lib/switchPractice.dart
@@ -0,0 +1,16 @@
+void main(){
+
+  int x = 10;
+  int y = 20;
+
+  var operation = '-';
+
+  switch(operation){
+    case '+':
+      print(x+y);
+      return;
+    case '-':
+      print(y-x);
+      return;
+  }
+}
