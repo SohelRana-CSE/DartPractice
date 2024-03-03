3215bb7a143f338ed82a243a2b51a3ec2b7f0fbb
Dart_Basic_Practice <soheldcc18@gmail.com>
2024-03-04 02:03:02 +0600

    initial commit

---
+++ lib/listPractice3.dart
@@ -0,0 +1,10 @@
+void main(){
+  dynamic data = [0, 'rana', 5, 6, 'sohel'];
+
+  for(int i = 0; i < data.length; i++){
+    print('The data is ${data[i]}');
+  }
+  print(data.length);
+  print(data.isEmpty);
+
+}
