527a25d88fa4af0e16e2cdbe58cf14c6785cc368
Dart_Basic_Practice <soheldcc18@gmail.com>
2024-03-05 04:04:37 +0600

    initial commit

---
+++ lib/switchCasePractice.dart
@@ -0,0 +1,24 @@
+void main(){
+
+  var marks = 71;
+
+  switch(marks){
+    case 80:
+    print('A+');
+    break;
+
+    case 70:
+      print("A");
+      break;
+
+    case 60:
+      print("A-");
+      break;
+
+    default:
+      print("Result Not Found");
+      break;
+  }
+
+
+}
