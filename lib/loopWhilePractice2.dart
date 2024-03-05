d9d7477603d6cf5c18183439b5d5630673631214
Dart_Basic_Practice <soheldcc18@gmail.com>
2024-03-06 01:09:53 +0600

    initial commit

---
+++ lib/loopWhilePractice2.dart
@@ -0,0 +1,26 @@
+void main(){
+  /*
+  for(var i = 0; i<100; i++){
+    print(i);
+  }
+  */
+
+
+  /*
+  var i = 0;
+  while(i<100){
+    print(i);
+    i = i+1;
+  }
+   */
+
+
+
+  var i=0;
+  do{
+    print(i);
+    i=i+1;
+  }while (i<100);
+
+
+}
