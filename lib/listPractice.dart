3cfe9eb7f0db4382a2b60ec8817f7f83ec1deeff
Dart_Basic_Practice <soheldcc18@gmail.com>
2024-03-06 00:30:57 +0600

    initial commit

--- lib/listPractice.dart
+++ lib/listPractice.dart
@@ -1,10 +1,21 @@
 void main(){
 
-  var city = ['Dhaka','Rangpur','Rajshahi', 'Chittagong', 'Sylhet'];
+  var city = ['Dhaka','Rangpur','Rajshahi', 2 , 'Chittagong', 'Sylhet'];
 
   print(city);
 
+  city.remove(2);
+  print(city);
+
+
+  print(city.length);
+
+  city.add("barishal");
+  print(city);
+
   print(city[3]);
+
+
 
 }
 
