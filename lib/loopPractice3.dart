c6508ffcb8e6b9dabbcbd4b878102409f66088f5
Dart_Basic_Practice <soheldcc18@gmail.com>
2024-03-15 00:17:57 +0600

    initial commit

---
+++ lib/loopPractice3.dart
@@ -0,0 +1,11 @@
+void main(){
+  for (int i = 0; i <= 50; i++){
+
+    if(i == 20){
+      print('value is checked');
+      break;    // if use continue then all numbers are print
+    }else{
+      print(i);
+    }
+  }
+}
