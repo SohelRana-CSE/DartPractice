57e65b95b8bfda08cae6ec038203ee5fe7547b1b
Dart_Basic_Practice <soheldcc18@gmail.com>
2024-04-02 00:05:42 +0600

    initial commit

---
+++ lib/functionPractice6.dart
@@ -0,0 +1,12 @@
+void main(){
+
+  var myName = myClass();
+  myName.printName();
+}
+
+class myClass{
+
+  void printName(){       //Declaration of function
+    print('Sohel Rana');  //Defination of function
+  }
+}
