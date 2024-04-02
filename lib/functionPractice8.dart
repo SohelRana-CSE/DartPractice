7a6a236fe544712613298abd0d43656475dcda70
Dart_Basic_Practice <soheldcc18@gmail.com>
2024-04-03 00:22:56 +0600

    initial commit

---
+++ lib/functionPractice8.dart
@@ -0,0 +1,13 @@
+//passing argument inside a function / parameter passing
+
+addTwoNumers(var x, y){
+  print(x + y);
+}
+
+void main(){
+  addTwoNumers(5, 6);
+  addTwoNumers(2, 6);
+  addTwoNumers(5, 9);
+  addTwoNumers(45, 36);
+
+}
